2afaf58c5095c6a8d078e1a947f2db43770fcdae
Oktawia Rogowicz <oktawia.r@fireart-d.com>
2025-08-13 15:46:02 +0200

    feat: (wip) add heroSection; add 'title', 'paragraph' and 'button' components

---
+++ studio/src/schemaTypes/objects/footer.ts
@@ -0,0 +1,73 @@
+import {defineField, defineType} from 'sanity'
+
+export const footer = defineType({
+  name: 'footer',
+  title: 'Footer',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'object',
+      fields: [
+        defineField({
+          name: 'contentBlocks',
+          title: 'Content Blocks',
+          type: 'array',
+          of: [
+            defineField({
+              name: 'contentBlock',
+              title: 'Content Block',
+              type: 'object',
+              fields: [
+                defineField({
+                  name: 'title',
+                  title: 'Title',
+                  type: 'string',
+                }),
+                defineField({
+                  name: 'description',
+                  title: 'Description',
+                  type: 'blockContent',
+                }),
+              ],
+            }),
+          ],
+        }),
+        defineField({
+          name: 'copyright',
+          title: 'Copyright',
+          type: 'string',
+        }),
+      ],
+    }),
+    defineField({
+      name: 'menu',
+      title: 'Menu',
+      type: 'array',
+      of: [
+        defineField({
+          name: 'menuItem',
+          title: 'Menu Item',
+          type: 'object',
+          fields: [
+            defineField({
+              name: 'label',
+              title: 'Label',
+              type: 'string',
+              validation: (Rule) => Rule.required(),
+            }),
+            defineField({
+              name: 'link',
+              title: 'Link',
+              type: 'link',
+              validation: (Rule) => Rule.required(),
+            }),
+          ],
+          validation: (Rule) => Rule.required(),
+        }),
+      ],
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+})
---
+++ studio/src/schemaTypes/objects/header.ts
@@ -0,0 +1,43 @@
+import {defineField, defineType} from 'sanity'
+
+export const header = defineType({
+  name: 'header',
+  title: 'Header',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'logo',
+      title: 'Logo',
+      type: 'media',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'menu',
+      title: 'Menu',
+      type: 'array',
+      of: [
+        defineField({
+          name: 'menuItem',
+          title: 'Menu Item',
+          type: 'object',
+          fields: [
+            defineField({
+              name: 'label',
+              title: 'Label',
+              type: 'string',
+              validation: (Rule) => Rule.required(),
+            }),
+            defineField({
+              name: 'link',
+              title: 'Link',
+              type: 'link',
+              validation: (Rule) => Rule.required(),
+            }),
+          ],
+          validation: (Rule) => Rule.required(),
+        }),
+      ],
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+})
