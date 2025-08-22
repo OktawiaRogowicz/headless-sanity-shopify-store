87ad337bc73640b45c80ebaeb1756cd269dfc427
Oktawia Rogowicz <oktawia.r@fireart-d.com>
2025-08-22 14:27:07 +0200

    feat: add internationalization to frontend + sanity

--- studio/src/structure/index.ts
+++ studio/src/structure/index.ts
@@ -130,14 +130,6 @@ export const structure: StructureResolver = (S: StructureBuilder) =>
   S.list()
     .title('Website Content')
     .items([
-      ...S.documentTypeListItems()
-        // Remove the "assist.instruction.context" and "settings" content  from the list of content types
-        .filter((listItem: any) => !DISABLED_TYPES.includes(listItem.getId()))
-        // Pluralize the title of each document type.  This is not required but just an option to consider.
-        .map((listItem) => {
-          return listItem.title(pluralize(listItem.getTitle() as string))
-        }),
-      // Settings Singleton in order to view/edit the one particular document for Settings.  Learn more about Singletons: https://www.sanity.io/docs/create-a-link-to-a-single-edit-page-in-your-main-document-type-list
       S.listItem()
         .title('Pages')
         .icon(DocumentsIcon)
