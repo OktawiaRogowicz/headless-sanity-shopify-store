fad60784765dfc1d2c3fc11501ea4572f4ddc502
Oktawia Rogowicz <oktawia.r@fireart-d.com>
2025-08-22 15:06:48 +0200

    fix: update HeroSection import type

--- frontend/src/sections/HeroSection.tsx
+++ frontend/src/sections/HeroSection.tsx
@@ -1,14 +1,13 @@
-import { HeroSection } from "@/sanity.types";
+import { HeroSection as HeroSectionType } from "@/sanity.types";
 import Title from "@/components/Title";
-import Button from "@/components/Button";
 
 type HeroSectionProps = {
-  block: HeroSection;
+  block: HeroSectionType;
   index: number;
 };
 
 export default function HeroSection({ block }: HeroSectionProps) {
-  const { title, link, backgroundImages } = { ...block };
+  const { title } = { ...block };
   return (
     <div className="h-screen w-full relative flex items-center justify-center">
       <div className="absolute top-0 left-0 w-full h-full grid grid-cols-2">
@@ -17,7 +16,6 @@ export default function HeroSection({ block }: HeroSectionProps) {
       </div>
       <div className="relative flex flex-col items-center justify-center">
         {title && <Title type="h1">{title}</Title>}
-        {link && <Button variant="outline-light">{link.label}</Button>}
       </div>
     </div>
   );
