03cbe3022701c47ed54bb55d2fddb903283608df
Oktawia Rogowicz <oktawia.r@fireart-d.com>
2025-11-09 19:56:07 +0100

    feat: update sanity version from 4.14 to 4.5

--- frontend/sanity.types.ts
+++ frontend/sanity.types.ts
@@ -570,6 +570,11 @@ export type SiteConfigurationQueryResult = {
     }>;
   } | null;
 };
+// Variable: getPageSlugsQuery
+// Query: *[_type == 'page' && slug.current == $slug]{    slug,  }
+export type GetPageSlugsQueryResult = Array<{
+  slug: Slug | null;
+}>;
 // Variable: getPageQuery
 // Query: *[_type == 'page' && slug.current == $slug && language == $locale][0]{    _id,    _type,    name,    slug,    heading,    subheading,    "pageBuilder": pageBuilder[]{      ...,      _type == "heroSection" => {        ...,      },    },    "translations": *[_type == "translation.metadata" && references(^._id)].translations[].value->{       title,       slug,       language    },  }
 export type GetPageQueryResult = {
@@ -625,6 +630,7 @@ declare module "@sanity/client" {
     "\n  *[_type == \"footer\" && language == $locale][0] {\n    ...,\n  }\n": FooterQueryResult;
     "\n  *[_type == \"header\" && language == $locale][0] {\n    ...,\n  }\n": HeaderQueryResult;
     "\n{\n    \"footer\": \n  *[_type == \"footer\" && language == $locale][0] {\n    ...,\n  }\n,\n    \"header\": \n  *[_type == \"header\" && language == $locale][0] {\n    ...,\n  }\n\n}\n": SiteConfigurationQueryResult;
+    "\n  *[_type == 'page' && slug.current == $slug]{\n    slug,\n  }\n": GetPageSlugsQueryResult;
     "\n  *[_type == 'page' && slug.current == $slug && language == $locale][0]{\n    _id,\n    _type,\n    name,\n    slug,\n    heading,\n    subheading,\n    \"pageBuilder\": pageBuilder[]{\n      ...,\n      _type == \"heroSection\" => {\n        ...,\n      },\n    },\n    \"translations\": *[_type == \"translation.metadata\" && references(^._id)].translations[].value->{\n       title,\n       slug,\n       language\n    },\n  }\n": GetPageQueryResult;
     "\n  *[_type == \"page\" || _type == \"post\" && defined(slug.current)] | order(_type asc) {\n    \"slug\": slug.current,\n    _type,\n    _updatedAt,\n  }\n": SitemapDataResult;
     "\n  *[_type == \"post\" && defined(slug.current)] | order(date desc, _updatedAt desc) {\n    \n  _id,\n  \"status\": select(_originalId in path(\"drafts.**\") => \"draft\", \"published\"),\n  \"title\": coalesce(title, \"Untitled\"),\n  \"slug\": slug.current,\n  excerpt,\n  coverImage,\n  \"date\": coalesce(date, _updatedAt),\n  \"author\": author->{firstName, lastName, picture},\n\n  }\n": AllPostsQueryResult;
